49aac06992a04ae7ee8a83cd5bbf367d1d0776a9
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2023-03-28 10:11:53 +0100

    NewStackV144

--- Novartis/CustomStack.js
+++ Novartis/CustomStack.js
@@ -40,7 +40,7 @@
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     // HTML extension with all necessary logic(s) wrtitten JS vvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv  
-    class NewStackV143 extends HTMLElement {
+    class NewStackV144 extends HTMLElement {
         constructor() {
             super()
 
@@ -1624,6 +1624,6 @@
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     // Return the end result into SAC (SAP ANALYTICS CLOUD) application vvvvvvvvvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    customElements.define('com-sap-sample-asantos-new-cwstackv1', NewStackV143)
+    customElements.define('com-sap-sample-asantos-new-cwstackv1', NewStackV144)
 
 })() // END of function --> (function () {
