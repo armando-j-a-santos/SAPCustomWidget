d67e79dbc49cc4d9a169b23dd640c67e2c5223e2
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2023-11-22 12:45:19 +0000

    Div1

--- BP/cw_main_table_v3.js
+++ BP/cw_main_table_v3.js
@@ -105,6 +105,9 @@
         div = document.createElement('div');
         div.slot = "content_" + widgetName;
 
+        let div1 = document.createElement('div');
+        div1.innerHTML = '<script src="https://openui5.hana.ondemand.com/1.108.20/resources/sap-ui-core.js" type="text/javascript" id="sap-ui-bootstrap" data-sap-ui-libs="sap.m,sap.ui.commons,sap.ui.table,sap.ui.layout,sap.ui.core.mvc.Controller" data-sap-ui-theme="sap_bluecrystal" data-sap-ui-bindingSyntax="complex" ui-compatVersion="edge" data-sap-ui-preload="async"> </script>'
+        _shadowRoot.appendChild(div1);
             
         let div2 = document.createElement('div');
         div2.innerHTML = '<script id="oView' + widgetName + '" name="oView' + widgetName + '" type="sapui5/xmlview"><mvc:View controllerName="myView.Template" xmlns:core="sap.ui.core" xmlns:mvc="sap.ui.core.mvc" xmlns:t="sap.ui.table" xmlns="sap.ui.commons" xmlns="sap.ui.layout" xmlns="sap.m"><t:TreeTable id="tbl" rows="{path:'/',parameters:{arrayNames:["data"]}}" ><t:columns><t:Column><t:label><Label text="name" /></t:label><t:template><TextView text="{name}" /></t:template></t:Column><t:Column><t:label><Label text="Arm" /></t:label><t:template><TextView text="{description}" /></t:template></t:Column><t:Column><t:label><Label text="product" /></t:label><t:template><TextView text="{product}" /></t:template></t:Column></t:columns></t:TreeTable></mvc:View></script>';
