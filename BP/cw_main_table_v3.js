7effcc1a9c705f2695b4aed258ae300a2c5541ff
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2023-11-23 11:39:30 +0000

    GetProperty

--- BP/cw_main_table_v3.js
+++ BP/cw_main_table_v3.js
@@ -258,8 +258,9 @@
 
                             //console.log(this.getView().byId("tbl").getModel());  // ---->> undefined
                             console.log(this.getView().getModel());  
-
-                            //console.log(this.getView().getModel().getProperty(sPath))
+                            
+                            
+                            console.log(this.getView().getModel().getProperty())
                             //console.log(this.getView().byId(that.widgetName).getModel()); // ---->> error
 
                             //"oViewsapuitable_1"
