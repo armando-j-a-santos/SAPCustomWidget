cfacbd6f4aacc7794475b88fd50e7542d2d57ab8
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2023-11-23 11:08:15 +0000

    asdfgh

--- BP/cw_main_table_v2.js
+++ BP/cw_main_table_v2.js
@@ -115,7 +115,7 @@
 
             
         let div2 = document.createElement('div');
-        div2.innerHTML = '<script id="oView' + widgetName + '" name="oView' + widgetName + '" type="sapui5/xmlview"><mvc:View controllerName="myView.Template" xmlns:core="sap.ui.core" xmlns:mvc="sap.ui.core.mvc" xmlns:t="sap.ui.table" xmlns="sap.ui.commons"><t:TreeTable id="tbl" rows="{path:\'/Hier/\'}"><t:columns><t:Column><t:label><Label text="ID" /></t:label><t:template><TextView text="{name}"/></t:template></t:Column><t:Column><t:label><Label text="NAME" /></t:label><t:template><TextView text="{description}"/></t:template></t:Column><t:Column><t:label><Label text="SURNAME" /></t:label><t:template><TextView text="{product}"/></t:template></t:Column></t:columns></t:TreeTable></mvc:View></script>';               
+        div2.innerHTML = '<script id="oView' + widgetName + '" name="oView' + widgetName + '" type="sapui5/xmlview"><mvc:View controllerName="myView.Template" xmlns:core="sap.ui.core" xmlns:mvc="sap.ui.core.mvc" xmlns:t="sap.ui.table" xmlns="sap.ui.commons"><t:TreeTable id="tbl" rows="{path:/Hier/}"><t:columns><t:Column><t:label><Label text="ID" /></t:label><t:template><TextView text="{name}"/></t:template></t:Column><t:Column><t:label><Label text="NAME" /></t:label><t:template><TextView text="{description}"/></t:template></t:Column><t:Column><t:label><Label text="SURNAME" /></t:label><t:template><TextView text="{product}"/></t:template></t:Column></t:columns></t:TreeTable></mvc:View></script>';               
         _shadowRoot.appendChild(div2);
        
         let div3 = document.createElement('div');
@@ -288,7 +288,7 @@
                             
                             // Link the model to the widget
                             this.getView().setModel(_oModel, that.widgetName);
-                            //sap.ui.getCore().setModel(_oModel, that.widgetName);
+                            
 
                             //console.log(this.getView().byId("tbl").getModel());  // ---->> undefined
                             //console.log(this.getView().byId(that.widgetName).getModel()); // ---->> error
