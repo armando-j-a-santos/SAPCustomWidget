549cd6507e1868138516dfaa683b426880d72e8e
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2023-11-28 15:03:28 +0100

    commit

--- BP/cw_tableTree_V2.js
+++ BP/cw_tableTree_V2.js
@@ -76,10 +76,11 @@
         // Only a SINGLE treetable should appear to the SAC user. For example, by appling different filters to the techTable behind the scenes.
         // The below while cycle will break when there is no __xmlview1,2,3,4,5,6... to to be removed.
         var i = 1;
-        while (true) {
+        while (i<100) {
             var x = document.getElementById(`__xmlview${i}`);
             if (x) {
                 document.getElementById(`__xmlview${i}`).remove();
+                console.log('deleted the element.');
                 break;
             }
             i++;
